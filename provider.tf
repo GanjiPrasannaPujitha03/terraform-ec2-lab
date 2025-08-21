d3f26a544dc66f3261b6a0b0ccc62d25b0c74000
GanjiPrasannaPujitha03 <prasannapujitha03@gmail.com>
2025-08-21 09:48:23 +0530

    Update provider.tf with new configuration

--- provider.tf
+++ provider.tf
@@ -1,3 +1,17 @@
+terraform {
+  #   backend "s3" {
+  #     bucket       = "pujitha-demobucket-12345"
+  #     key          = "terraform.tfstate"
+  #     region       = "eu-west-3"
+  #     use_lockfile = true
+  #   }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
 provider "aws" {
   region = "eu-west-3" # Paris region (change if needed)
 }
